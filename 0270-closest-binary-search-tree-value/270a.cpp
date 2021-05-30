3929ce96db35fd0b83f95107b873a3f95c6be762
Robbie Nohra <rnohra@benchsci.com>
2021-05-30 13:09:10 -0400

    update: 270

---
+++ 0270-closest-binary-search-tree-value/270a.cpp
@@ -0,0 +1,54 @@
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  void helper(TreeNode *root, vector<int> &res) {
+    if (root != nullptr) {
+      if (root->left != nullptr) {
+        helper(root->left, res);
+      }
+
+      res.push_back(root->val);
+      if (root->right != nullptr) {
+        helper(root->right, res);
+      }
+    }
+  }
+  vector<int> inorderTraversal(TreeNode *root) {
+
+    vector<int> res;
+
+    helper(root, res);
+
+    return res;
+  }
+  int closestValue(TreeNode *root, double target) {
+    vector<int> res = inorderTraversal(root);
+
+    int min = res[0];
+
+    vector<int>::iterator it;
+
+    for (it = res.begin(); it != res.end(); it++) {
+      if (abs(static_cast<float>(*it) - target) <
+          abs(static_cast<float>(min) - target)) {
+        min = *it;
+      }
+    }
+
+    return min;
+  }
+};
